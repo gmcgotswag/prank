2ceac46533a1a20396cf12bca871e4b688a92bd8
gmcgotswag <gmc1314@outlook.com>
2017-05-18 18:07:22 -0500

    this is the file
    try at risk iam not reliable for what happened you chose to download no viruses just a cool little program for cmd

---
+++ i did it for the lulz.bat
@@ -0,0 +1,44 @@
+@echo off
+color 0a
+pause
+echo iam a computer that going to mess with your head i might open browser or that kind of stuff just dont be scared ok iam not a virus ill be gone in 5 mins
+pause
+echo but seriously iam a.i and im bored so iam going to check out google
+pause 
+start https://www.google.com
+pause 
+echo  lets check out youtube
+pause
+start https://www.youtube.com
+pause
+echo ok lets make you laugh and you will watch this stuff or else 
+pause
+start https://www.youtube.com/user/BYUTelevision
+echo lol 
+pause 
+echo oh there hillarious
+pause
+echo ok let play some death run try not to die
+pause 
+start http://www.y8.com/games/death_run_3d
+echo may I suggest you play something withing your range of gameing abilty like the slope game.
+pause 
+echo ofcourse you dont have an option so 
+pause
+start http://www.y8.com/games/slope
+echo are you having fun yet.
+pause 
+echo ok ill stop after one more game ok
+pause
+echo but you have to get at least 5
+pause 
+echo can you guess the game
+pause
+echo if you guessed flappy bird than yo must hate your life
+pause
+start https://flappybird.me/
+pause 
+echo did you get 5
+pause 
+all condolences who created me 
+pause
